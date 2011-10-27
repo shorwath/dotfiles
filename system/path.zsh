acb47adae6f40e19132394ffe0b1f426ac1138fc
Scott Horwath <shorwath@ign.com>
2011-10-27 15:36:30 -0700

    fixed path file and .zshrc to mimic previous setup

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,2 @@
-export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
+export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:/usr/bin:/usr/games:/usr/sbin:/sbin:/home/shorwath/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
